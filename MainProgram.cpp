e0181c88dea9c772434271c51b59d01dc8874784
alenmihciyan <alen.mihciyan@bahcesehir.edu.tr>
2026-04-14 12:50:35 +0300

    Update MainProgram.cpp

--- MainProgram.cpp
+++ MainProgram.cpp
@@ -133,14 +133,14 @@ public:
     // Two students are equal if they have the same id
     bool operator==(const Student& other) const {
         // YOUR CODE HERE
-        return id==other.id;
+        return id == other.id;
     }
 
     // TODO 5b: Less-than operator (<)
     // Compare by GPA (lower GPA = "less than")
     bool operator<(const Student& other) const {
         // YOUR CODE HERE
-        return gpa<other.gpa;
+        return gpa < other.gpa;
     }
 
     // TODO 5c: Stream insertion operator (<<)
